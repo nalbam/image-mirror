2897bf93c6713ecfefc35fcdbe2c7cfbbfdda4dd
nalbam <me@nalbam.com>
2022-02-24 21:35:04 +0900

    repository_dispatch

---
+++ report.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+SHELL_DIR=$(dirname $0)
+
+DEFAULT="nalbam/image-mirror"
+REPOSITORY=${GITHUB_REPOSITORY:-$DEFAULT}
+
+USERNAME=${GITHUB_ACTOR}
+REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
+
+_init() {
+  rm -rf ${SHELL_DIR}/.previous
+
+  mkdir ${SHELL_DIR}/target
+
+  cp -rf ${SHELL_DIR}/versions ${SHELL_DIR}/.previous
+}
+
+_check() {
+  # check versions
+  while read LINE; do
+    _get_versions ${LINE}
+  done <${SHELL_DIR}/checklist.txt
+}
+
+_get_versions() {
+  CHART="$1"
+
+  BASE_IMAGE="${2}"
+  IMAGE_NAME="${3}"
+  PLATFORM="${4:-"linux/amd64,linux/arm64"}"
+  BUILDX="${5:-"true"}"
+
+  curl -sL https://api.github.com/repos/${CHART}/releases | jq '.[].tag_name' -r | grep -v '-' | head -10 \
+    >${SHELL_DIR}/versions/${NAME}
+
+  while read V1; do
+    if [ -z "$V1" ]; then
+      continue
+    fi
+
+    EXIST="false"
+    while read V2; do
+      if [ "$V1" == "$V2" ]; then
+        EXIST="true"
+        # echo "# ${NAME} ${V1} EXIST"
+        continue
+      fi
+    done <${SHELL_DIR}/.previous/${NAME}
+
+    if [ "$EXIST" == "false" ]; then
+      # send slack message
+      _slack "$V1"
+    fi
+  done <${SHELL_DIR}/versions/${NAME}
+}
+
+_slack() {
+  if [ -z "${SLACK_TOKEN}" ]; then
+    return
+  fi
+
+  VERSION="$1"
+
+  EVENT_TYPE="mirror"
+
+  curl -sL -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -d "{\"event_type\":\"${EVENT_TYPE}\",\"client_payload\":{\"base_image\":\"${BASE_IMAGE}\",\"image_name\":\"${IMAGE_NAME}\",\"tag_name\":\"${VERSION}\",\"platform\":\"${PLATFORM}\",\"buildx\":\"${BUILDX}\"}}" \
+    https://api.github.com/repos/${REPOSITORY}/dispatches
+
+  echo "# dispatch ${CHART} ${VERSION}"
+}
+
+_message() {
+  # commit message
+  printf "$(date +%Y%m%d-%H%M)" >${SHELL_DIR}/target/commit_message.txt
+}
+
+_run() {
+  _init
+
+  _check
+
+  _message
+}
+
+_run
