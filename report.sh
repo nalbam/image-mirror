090aa373cd8d13e922cb739c15efafc198f20e79
nalbam <me@nalbam.com>
2022-02-24 21:52:16 +0900

    versions

--- report.sh
+++ report.sh
@@ -34,25 +34,29 @@ _get_versions() {
   curl -sL https://api.github.com/repos/${REPO}/releases | jq '.[].tag_name' -r | grep -v '-' | head -10 \
     >${SHELL_DIR}/versions/${NAME}
 
-  while read V1; do
-    if [ -z "$V1" ]; then
-      continue
-    fi
-
-    EXIST="false"
-    while read V2; do
-      if [ "$V1" == "$V2" ]; then
-        EXIST="true"
-        # echo "# ${NAME} ${V1} EXIST"
+  if [ -f ${SHELL_DIR}/versions/${NAME} ]; then
+    while read V1; do
+      if [ -z "$V1" ]; then
         continue
       fi
-    done <${SHELL_DIR}/.previous/${NAME}
 
-    if [ "$EXIST" == "false" ]; then
-      # send slack message
-      _slack "$V1"
-    fi
-  done <${SHELL_DIR}/versions/${NAME}
+      EXIST="false"
+      if [ -f ${SHELL_DIR}/.previous/${NAME} ]; then
+        while read V2; do
+          if [ "$V1" == "$V2" ]; then
+            EXIST="true"
+            # echo "# ${NAME} ${V1} EXIST"
+            continue
+          fi
+        done <${SHELL_DIR}/.previous/${NAME}
+      fi
+
+      if [ "$EXIST" == "false" ]; then
+        # send slack message
+        _slack "$V1"
+      fi
+    done <${SHELL_DIR}/versions/${NAME}
+  fi
 }
 
 _slack() {
